5f70c6e916e9d1a6a087c1ac21a3d3491cd8c6e2
Bogdan Anton <contact@bogdananton.ro>
2015-08-17 11:14:34 +0300

    Update EditorDetails.java

--- src/ro/bogdananton/testNameGenerator/settings/EditorDetails.java
+++ src/ro/bogdananton/testNameGenerator/settings/EditorDetails.java
@@ -39,7 +39,8 @@ public class EditorDetails
 
     public static String getLineDelimiter()
     {
-        return SystemProperties.getLineSeparator();
+        return "\n";
+        // return SystemProperties.getLineSeparator();
     }
 }
 
